c8b482763f3fe97b16a2053f0e0ff8454491f321
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-04-20 18:25:21 -0600

    not working

--- figma-imports/icons.js
+++ figma-imports/icons.js
@@ -1,23 +1,25 @@
-const path = require('path');
-const fs = require('fs');
-const {exec} = require('child_process');
-const {pascalCase} = require('change-case');
-const Promise = require('bluebird');
+const path = require( "path" );
+const fs = require( "fs" );
+const { exec } = require( "child_process" );
+const { pascalCase } = require( "change-case" );
+const Promise = require( "bluebird" );
 
-const {api, getIconJSXTemplate, svgo} = require('./utils');
+const { api, getIconJSXTemplate, svgo } = require( "./utils" );
 
-const IconsDir = path.resolve(__dirname, '../src/components/Icons');
+const IconsDir = path.resolve( __dirname, "../src/components/Icons" );
 
-const getIconFolderPath = (name) => path.resolve(IconsDir, pascalCase(name));
+const getIconFolderPath = ( name ) => path.resolve( IconsDir,
+  pascalCase( name ),
+);
 
-const writeFile = Promise.promisify(fs.writeFile);
+const writeFile = Promise.promisify( fs.writeFile );
 
 /**
  * clear icons dir except Icon.jsx and Icon.css files
  *
  */
 const clearIconsDir = () => {
-  exec(`rm -rf ${IconsDir} -v !(Icon.jsx) !(Icon.css)`);
+  exec( `rm -rf ${ IconsDir } -v !(Icon.jsx) !(Icon.css)` );
 };
 
 /**
@@ -27,27 +29,32 @@ const clearIconsDir = () => {
  * @param iconNode
  * @return {Promise<void>}
  */
-const generateIcon = async (iconNode) => {
-  const iconUrl = await api.getSvgImageUrl(iconNode.id);
-
-  const iconName = pascalCase(iconNode.name);
-  const iconFolderPath = getIconFolderPath(iconName);
-
-  if (!fs.existsSync(iconFolderPath)) {
-    fs.mkdirSync(iconFolderPath);
+const generateIcon = async( iconNode ) => {
+  const iconUrl = await api.getSvgImageUrl( iconNode.id );
+  
+  const iconName = pascalCase( iconNode.name );
+  const iconFolderPath = getIconFolderPath( iconName );
+  
+  if( !fs.existsSync( iconFolderPath ) ){
+    fs.mkdirSync( iconFolderPath );
   }
-
-  const {data: iconContent} = await api.getImageContent(iconUrl);
-  const {data: optimizedIconContent} = await svgo.optimize(iconContent);
-
-  const iconJSXTemplate = getIconJSXTemplate(iconName);
-
-  await Promise.all([
-    writeFile(path.resolve(iconFolderPath, `${iconName}.svg`), optimizedIconContent, {encoding: 'utf8'}),
-    writeFile(path.resolve(iconFolderPath, `${iconName}.jsx`), iconJSXTemplate, {encoding: 'utf8'})
-  ]);
-
-  console.log(`${iconName} was written success!`);
+  
+  const { data: iconContent } = await api.getImageContent( iconUrl );
+//  const {data: optimizedIconContent} = await svgo.optimize(iconContent);
+  
+  const iconJSXTemplate = getIconJSXTemplate( iconName );
+  
+  await Promise.all( [
+    writeFile( path.resolve( iconFolderPath, `${ iconName }.svg` ),
+      iconContent,
+      { encoding: "utf8" },
+    ), writeFile( path.resolve( iconFolderPath, `${ iconName }.jsx` ),
+      iconJSXTemplate,
+      { encoding: "utf8" },
+    ),
+  ] );
+  
+  console.log( `${ iconName } was written success!` );
 };
 
 /**
@@ -56,10 +63,10 @@ const generateIcon = async (iconNode) => {
  * @param {[Object]} iconNodesArr - array of icon nodes from frame
  * @return {Promise<void>}
  */
-const generateIcons = async (iconNodesArr) => {
-  await Promise.map(iconNodesArr, generateIcon, {
-    concurrency: Number.parseInt(process.env.CONCURRENCY),
-  });
+const generateIcons = async( iconNodesArr ) => {
+  await Promise.map( iconNodesArr, generateIcon, {
+    concurrency: Number.parseInt( process.env.CONCURRENCY ),
+  } );
 };
 
 /**
@@ -67,31 +74,30 @@ const generateIcons = async (iconNodesArr) => {
  *
  * @param iconNodesArr - array of icon nodes from frame
  */
-const generateImports = (iconNodesArr) => {
-  const fileWithImportsPath = path.resolve(IconsDir, 'index.js');
-
+const generateImports = ( iconNodesArr ) => {
+  const fileWithImportsPath = path.resolve( IconsDir, "index.js" );
+  
   const importsContent = iconNodesArr
-    .map(iconNode => {
-      const iconName = pascalCase(iconNode.name);
-
-      return `export * from './${iconName}/${iconName}';`;
-    })
-    .join('\n');
-
-  fs.writeFileSync(fileWithImportsPath, importsContent, {encoding: 'utf8'});
-
-  console.log(`imports was written success!`);
+    .map( iconNode => {
+      const iconName = pascalCase( iconNode.name );
+      
+      return `export * from './${ iconName }/${ iconName }';`;
+    } )
+    .join( "\n" );
+  
+  fs.writeFileSync( fileWithImportsPath, importsContent, { encoding: "utf8" } );
+  
+  console.log( `imports was written success!` );
 };
 
-const main = async () => {
+const main = async() => {
   clearIconsDir();
-
-  const iconNodesArr = await api.getNodeChildren(process.env.FRAME_WITH_ICONS_ID);
-
-  await Promise.all([
-    generateIcons(iconNodesArr),
-    generateImports(iconNodesArr)
-  ])
+  
+  const iconNodesArr = await api.getNodeChildren( process.env.FRAME_WITH_ICONS_ID );
+  
+  await Promise.all( [
+    generateIcons( iconNodesArr ), generateImports( iconNodesArr ),
+  ] );
 };
 
 module.exports = main;
--- figma-imports/utils/getIconJSXTemplate.js
+++ figma-imports/utils/getIconJSXTemplate.js
@@ -1,23 +1,39 @@
-const getGenFileCaption = require('./getGenFileCaption');
+const getGenFileCaption = require( "./getGenFileCaption" );
 
 /**
  * get icon component template
  *
  * @param {string} name
  */
-module.exports = (name) => `
-${getGenFileCaption()}
+module.exports = ( name ) => `
+${ getGenFileCaption() }
 import React from 'react';
+import styled, {keyframes} from 'styled-component';
 
-import {ReactComponent as ${name}Component} from './${name}.svg';
+import {ReactComponent as ${ name }Component} from './${ name }.svg';
 
 import Icon from '../Icon';
 
-export const ${name} = (props) => (
+export const ${ name } = (props) => (
   <Icon {...props}>
-    <${name}Component/>
+    <${ name }Svg/>
   </Icon>
 );
 
-${name}.propTypes = Icon.propTypes;
+const rotate = keyframes\`
+  0%{
+  rotate(0);
+  }100%{
+  rotate(360deg);
+}
+
+\`
+
+const ${ name }Svg = styled(${ name })\`
+ #rotate {
+    animation: 2s linear;
+    }
+
+
+\`;
 `;
